317d1b83914d38f723cd847aabbeefea28570635
Ryan (Chao) Deng <cdeng@successfactors.com>
2013-04-07 00:08:11 +0800

    second commit

---
+++ context.js
@@ -0,0 +1,14 @@
+<!-- var greeting = "hola, ";
+var button = document.getElementById("su");
+
+button.addEventListener("click", function() {
+  alert(greeting + ".");
+}, false); -->
+function genericOnClick(info, tab) {
+  alert(info.linkUrl);
+}
+function selectionOnClick(info, tab) {
+  alert(info.selectionText);
+}
+var link = chrome.contextMenus.create({"title": "链接地址","contexts":["link"],"onclick":genericOnClick});
+var selection = chrome.contextMenus.create({"title": "选中文字","contexts":["selection"],"onclick":selectionOnClick});
