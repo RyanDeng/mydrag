b571192d0edbe4690b696fa8107e7aa53e85a727
Ryan (Chao) Deng <cdeng@successfactors.com>
2013-04-07 17:59:30 +0800

    do

--- context.js
+++ context.js
@@ -1,14 +1,51 @@
-<!-- var greeting = "hola, ";
-var button = document.getElementById("su");
+// var greeting = "hola, ";
+// var button = document.getElementById("su");
 
-button.addEventListener("click", function() {
-  alert(greeting + ".");
-}, false); -->
-function genericOnClick(info, tab) {
-  alert(info.linkUrl);
+// button.addEventListener("click", function() {
+//   alert(greeting + ".");
+// }, false);
+// document.onmousedown=function(){
+//     alert("aaa");
+// };
+var kw=document.getElementById("kw");
+document.ondrag = function(e){
+    
+        //kw.value+="drag"
+    
 }
-function selectionOnClick(info, tab) {
-  alert(info.selectionText);
+// document.ondblclick = function(e){
+//     //var selectobj = document.selection.createRange();
+//     var selectstr = document.getSelection();
+//     alert(selectstr);
+// }
+document.ondragend =function(e){
+    //kw.value+="end,"
+    searchtxt=e.dataTransfer.getData("Text");
+    alert(searchtxt);
+    window.open(searchtxt);
 }
-var link = chrome.contextMenus.create({"title": "链接地址","contexts":["link"],"onclick":genericOnClick});
-var selection = chrome.contextMenus.create({"title": "选中文字","contexts":["selection"],"onclick":selectionOnClick});
+
+
+document.ondragstart = function(e){
+    // var selectobj = document.selection.createRange();
+    // var selectstr = selectobj.text;
+    // alet(selectstr);
+    var selectstr = document.getSelection();
+    kw.value+=selectstr;
+    e.dataTransfer.setData("Text",selectstr);
+}
+document.ondrag = function(e){
+    //kw.value+="drag,"
+}
+document.ondrop = function(e){
+    kw.value+="drop,"
+}
+
+
+
+document.onmousemove = function(e){
+        //div.innerText = '('+e.pageX +', '+e.pageY+')';
+}
+document.onmouseup = function(e){
+            //window.open("www.baidu.com");
+}
